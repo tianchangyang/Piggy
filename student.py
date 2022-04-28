2d14ba76830dbde95df5131745d907dcd8bc74c7
Tianchang Yang <102991146+tianchangyang@users.noreply.github.com>
2022-04-28 19:15:11 +0000

    add mike

--- student.py
+++ student.py
@@ -40,7 +40,7 @@ class Piggy(PiggyParent):
                 "s": ("Shy", self.shy),
                 "f": ("Follow", self.follow),
                 "c": ("Calibrate", self.calibrate),
-                "q": ("Quit", self.quit)
+                "q": ("Quit", self.quit),
                 "y": ("mike Test", self.mike)
                  }
         # loop and print the menu...
