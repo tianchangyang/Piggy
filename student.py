f0e6e18211d26bbe88b642e8846adb74c6f8df22
Tianchang Yang <102991146+tianchangyang@users.noreply.github.com>
2022-05-19 18:34:04 +0000

    dance

--- student.py
+++ student.py
@@ -95,24 +95,36 @@ class Piggy(PiggyParent):
 
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
-        # TODO: check to see if it's safe before dancing
+        if safe_to_self.dance():
         
-        # lower-ordered example...
-        self.right(primary=50, counter=50)
-        time.sleep(2)
-        self.left()
-        time.sleep(2)
-        self.fwd()
-        time.sleep(2)
-        self.right()
-        time.sleep(2)
-        self.back()
-        time.sleep(2)
-        self.stop()
+        
+          self.right(primary=50, counter=50)
+          time.sleep(2)
+          self.left()
+          time.sleep(2)
+          self.fwd()
+          time.sleep(2)
+          self.right()
+          time.sleep(2)
+          self.back()
+          time.sleep(2)
+          self.stop()
 
     def safe_to_dance(self):
         """ Does a 360 distance check and returns true if safe """
-        pass
+      self.read_distance()
+      self.servo(1000)
+      time.sleep(2)
+      if self.read_distance() < 500:
+        return False
+      self.read_distance()
+      self.servo(2000)
+      time.sleep(2)
+      if self.read_distance() < 500:
+        return False
+
+      return True
+      
 
     def shake(self):
         """ Another example move """
