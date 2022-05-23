306f49e6be58d3d5f3587519ff4ad8e1b79ba606
Tianchang Yang <102991146+tianchangyang@users.noreply.github.com>
2022-05-23 18:56:42 +0000

    wall

--- student.py
+++ student.py
@@ -132,7 +132,7 @@ class Piggy(PiggyParent):
         if self.read_distance() < 500:
           self.stop()
           self.right(primary=90, counter=-90)
-          time.sleep()
+          time.sleep(1)
         else:
           self.fwd()
 
